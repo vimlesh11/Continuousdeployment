aeddfebf9126bf19101e424015abbd8884a7f2fe
vimlesh11 <vimlesh.singhit08@gmail.com>
2021-05-18 07:41:35 +0100

    Added new solution

---
+++ Continnousdeployment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Continnousdeployment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
